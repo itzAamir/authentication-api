320770ebe2030c50ff7f71436d4e544a80474c88
itzAamir <amirpc190320@gmail.com>
2021-04-10 15:29:45 +0530

    updated loginPage

--- server/routes/login.js
+++ server/routes/login.js
@@ -3,10 +3,19 @@ const loginRouter = express.Router();
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 
-loginRouter.post("/", (req, res) => {
+loginRouter.post("/", async (req, res) => {
     const { email, password } = req.body;
-    console.log(email, password);
-    res.send("not ok")
+    try {
+        const user = await User.findOne({ email })
+        const isValid = await bcrypt.compare(password, user.password)
+        if (isValid) {
+            res.json({ message: "ok", data: user })
+        } else {
+            res.json({ message: "error", data: "Invalid email or password" })
+        }
+    } catch (error) {
+        res.json({ message: "error", data: error.message })
+    }
 })
 
 module.exports = loginRouter;
